f3367162cb45b2027474eb668adf8941dfc6ae71
Elena Gherman <elena.gherman@ctbav.ro>
2021-07-16 12:45:20 +0300

    Add tema1-final version

--- Tema1/src/ro/ctbav/Main.java
+++ Tema1/src/ro/ctbav/Main.java
@@ -1,7 +1,26 @@
 package ro.ctbav;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-	    
+	    int age;
+
+	    System.out.print("Introduceti varsta: ");
+
+	    Scanner keyboard = new Scanner(System.in);
+	    age = keyboard.nextInt();
+
+	    if (age < 5) {
+            System.out.println("You are in kindergarten.");
+        } else if (age < 10) {
+            System.out.println("You are in elementary school.");
+        } else if (age < 13) {
+            System.out.println("You are in middle school.");
+        } else if (age < 18) {
+            System.out.println("You are in high school.");
+        } else {
+            System.out.println("You should be in college.");
+        }
     }
 }
